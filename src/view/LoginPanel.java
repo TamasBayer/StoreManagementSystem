4a1ea982f2bb1594a37ff33782624285dea9d0f7
Tamas Bayer <pititamas@gmail.com>
2020-03-09 22:10:14 +0100

    Login Panel prototype

---
+++ src/view/LoginPanel.java
@@ -0,0 +1,118 @@
+package view;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+public class LoginPanel extends JPanel{
+	
+	private JLabel userNameLabel;
+    private JTextField userNameField;
+    private JLabel passwordLabel;
+    private JTextField passwordField;
+    private JButton loginButton;
+    
+    String[] textFields;
+        
+    public LoginPanel(){
+        
+    	userNameLabel = new JLabel("User name:"); 
+    	userNameField = new JTextField(10);
+    	passwordLabel = new JLabel("Password: ");
+    	passwordField = new JTextField(10);
+    	loginButton = new JButton("Login");
+        
+        
+        Border innerBorder = BorderFactory.createLineBorder(Color.GRAY);
+        Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+        
+        layoutComponents();
+    }
+    
+    
+    public String[] getTextFields(){
+      
+        textFields = null;
+        
+        
+      if(!userNameField.getText().isEmpty() && !passwordField.getText().isEmpty() && !passwordField.getText().isEmpty() && !passwordField.getText().isEmpty()){
+        textFields = new String[4];
+          
+        textFields[0] = userNameField.getText();
+        textFields[1] = passwordField.getText();
+
+        
+      } 
+          
+        return textFields;
+    } 
+    
+    public JButton getLoginButton() {
+		return loginButton;
+	}
+    
+     public void layoutComponents(){
+        setLayout(new GridBagLayout());
+        
+        GridBagConstraints gc = new GridBagConstraints();
+        
+        /////////// First row /////////////////
+     
+        gc.gridy = 0;
+        
+        gc.weightx = 1;
+        gc.weighty = 0;
+        
+        gc.gridx = 0;
+        gc.fill = GridBagConstraints.NONE;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0, 0, 20, 5);
+        add(userNameLabel, gc);
+        
+        gc.gridx = 1;
+        gc.gridy = 0;
+        gc.insets = new Insets(0, 0, 20, 0);
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(userNameField, gc);
+        
+        /////////// Second row /////////////////
+        gc.gridy++;
+        
+        gc.weightx = 1;
+        gc.weighty =  0;
+        
+        gc.gridx = 0;
+        gc.insets = new Insets(0, 0 , 20, 5);
+        gc.anchor = GridBagConstraints.LINE_END;
+        add(passwordLabel, gc);
+        
+        gc.gridy = 1;
+        gc.gridx = 1;
+        gc.insets = new Insets(0, 0, 20, 0);
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(passwordField, gc);
+        
+        /////////// Second row /////////////////
+        
+	    gc.gridy++;
+	      
+	    gc.weightx = 1;
+	    gc.weighty =  0;
+	      
+        gc.gridx = 1;
+        gc.insets = new Insets(0, 50, 0, 0);
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(loginButton, gc);
+        
+     }
+
+}
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -2,26 +2,53 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
     
+	private LoginPanel loginPanel;
+	private JButton loginBtn;
 
     public MainFrame(){
         
-        super("ViewProject");
-        setSize(1000, 600);
-        setMinimumSize( new Dimension(650, 450));
+        super("Login");
+        setSize(400, 300);
+        setMinimumSize( new Dimension(400, 300));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
         
+        loginPanel = new LoginPanel();
+        loginBtn = loginPanel.getLoginButton();
+        
         setLayout(new BorderLayout());
 
-        add(new TabbedPane(), BorderLayout.CENTER);
+        add(loginPanel, BorderLayout.CENTER);
         
+        loginButtonPressed();
         
-        
+    }
+    
+    public void loginButtonPressed(){
+    	loginBtn.addActionListener(new ActionListener(){
+
+            
+            public void actionPerformed(ActionEvent e) {
+                
+            	setSize(1100, 700);
+                setMinimumSize( new Dimension(600, 400));
+                setLocationRelativeTo(null);
+                
+                remove(loginPanel);
+                add(new TabbedPane(), BorderLayout.CENTER);
+                
+                
+            }
+        });
+
     }
 }
